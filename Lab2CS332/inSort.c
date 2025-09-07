6dd24c4b4a24561b609784dba7fb0de7702aac50
Jon <135755963+SoleP12@users.noreply.github.com>
2025-09-07 00:02:07 +0000

    functionality for Lab2CS332 8:02pm 9/6

---
+++ Lab2CS332/inSort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void insertSort(){
+
+}
+
+int main(){
+    int N;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &N); //Expects pointer to an address to unpack
+    int sampleArray[N];
+    printf("Your array is %d elements: \n" , N); //Expects actual value
+    printf("Capacity of the array :%lu\n", sizeof(sampleArray)/sizeof(sampleArray[0]));
+
+    // printf();
+
+    // Insertion Sort
+
+    // printf(N);
+    return 0;
+}
+// gcc -o inSort inSort.c
+// ./inSort
