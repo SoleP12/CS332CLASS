33e915dca96cfe89f83c4a4b2d932215060b1e72
Jon <135755963+SoleP12@users.noreply.github.com>
2025-09-08 16:49:47 +0000

    adeded creation array

--- Lab2CS332/inSort.c
+++ Lab2CS332/inSort.c
@@ -2,9 +2,6 @@
 #include <time.h>
 #include <stdlib.h>
 
-void insertSort(){
-
-}
 
 int main(){
     int N;
@@ -12,12 +9,21 @@ int main(){
     printf("Enter the size of the array: ");
     scanf("%d", &N); //Expects pointer to an address to unpack
     int sampleArray[N];
-    printf("Your array is %d elements: \n" , N); //Expects actual value
-    printf("Capacity of the array :%lu\n", sizeof(sampleArray)/sizeof(sampleArray[0]));
+    printf("Your array is %d elements \n" , N); //Expects actual value
+    printf("Capacity of the array : %lu\n", sizeof(sampleArray)/sizeof(sampleArray[0]));
 
-    // printf(); sd
 
     // Insertion Sort
+    void creationRandomArray(){ 
+        for(int i = 0; i < N ; i++ ) {
+            int value = rand() % 30;
+            printf("%d ",value);
+        }
+    }
+
+
+
+
 
     // printf(N);
     return 0;
