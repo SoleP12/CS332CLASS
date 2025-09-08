5b0c69ead9ce3a91f0735d7f180a101f4ff3480e
Jon <135755963+SoleP12@users.noreply.github.com>
2025-09-08 17:42:58 +0000

    added completed Lab2CS332

--- Lab2CS332/inSort.c
+++ Lab2CS332/inSort.c
@@ -15,13 +15,28 @@ int main(){
     printf("Capacity of the array : %lu\n", sizeof(sampleArray)/sizeof(sampleArray[0]));
 
      srand(time(NULL)); 
-    
+    printf("Here is your unsorted Array: ");
     // Insertion Sort 
     for(int i = 0; i < N ; i++ ) {
         sampleArray[i] = rand() % 999;
         printf("%d ", sampleArray[i]);
     }
     printf("\n");
+    printf("Here is your sorted array: ");
+    int temporaryVal, currentLocal;
+    for (int i = 1; i < N; i++){
+        currentLocal = i;
+        while (currentLocal > 0 && sampleArray[currentLocal-1]> sampleArray[currentLocal]){
+            temporaryVal = sampleArray[currentLocal];
+            sampleArray[currentLocal] = sampleArray[currentLocal - 1];
+            sampleArray[currentLocal-1] = temporaryVal;
+            currentLocal--;
+        }
+    }
+    for(int i = 0; i<N; i++){
+        printf("%d ", sampleArray[i]);
+    }
+
 
     
 
