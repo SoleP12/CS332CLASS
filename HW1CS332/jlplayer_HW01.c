dc91623081009dc3c90886c98722daf31962ea08
Jon <135755963+SoleP12@users.noreply.github.com>
2025-09-14 03:22:48 +0000

    added more to HMW1

---
+++ HW1CS332/jlplayer_HW01.c
@@ -0,0 +1,78 @@
+// gcc -o jlplayer_HW01 jlplayer_HW01.c
+// ./jlplayer_HW01
+#include <stdio.h>
+
+// Write the function sumOfDigits that takes a positive integer n and returns an int which is
+//the sum of its digits.
+// • If n is less than or equal to 0, return "-1"
+ int sumOfDigits(){
+        printf("Hello");
+    }
+
+/////////////////////////////////////////////////////////////////////////
+// Write the function UABMaxMinDiff that takes an array of integers arr and returns the difference
+// between the maximum and minimum elements in the array. You can pass the size of the array as an
+// input parameter.
+int UABMaxMinDiff(){
+    printf("Hello");
+}
+
+/////////////////////////////////////////////////////////////////////////
+// Write the function replaceEvenWithZero that takes an array of integers arr and returns a
+// new array. The function should replace every even number in the array with 0. You can pass
+// the size of the array as an input parameter
+int replaceEvenWithZero(){
+    printf("Hello");
+}
+/////////////////////////////////////////////////////////////////////////
+// Write the function perfectSquare that takes an integer n and returns True if n is a perfect
+// square, and False otherwise. A perfect square is a number that can be expressed as k * k for
+// some integer k.
+int perfectSquare(){
+    printf("Hello");
+}
+
+/////////////////////////////////////////////////////////////////////////
+// Write the function countVowels that takes a string s and returns an integer. The function should count
+// the number of vowels (a, e, i, o, u) in the string, ignoring case.
+int countVowels(){
+    printf("Hello");
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+int main(){
+
+    sumOfDigits();
+    sumOfDigits();
+    sumOfDigits();
+    sumOfDigits();
+    sumOfDigits();
+
+    UABMaxMinDiff();
+    UABMaxMinDiff();
+    UABMaxMinDiff();
+    UABMaxMinDiff();
+    UABMaxMinDiff();
+
+    replaceEvenWithZero();
+    replaceEvenWithZero();
+    replaceEvenWithZero();
+    replaceEvenWithZero();
+    replaceEvenWithZero();
+
+    perfectSquare();
+    perfectSquare();
+    perfectSquare();
+    perfectSquare();
+    perfectSquare();
+
+    countVowels();
+    countVowels();
+    countVowels();
+    countVowels();
+    countVowels();
+
+
+
+}
