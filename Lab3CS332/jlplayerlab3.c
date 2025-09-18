c069d28e077c4f4ac1d175d0356d17e9ae673280
Jon <135755963+SoleP12@users.noreply.github.com>
2025-09-18 05:25:46 +0000

    r

--- Lab3CS332/jlplayerlab3.c
+++ Lab3CS332/jlplayerlab3.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
+void stringreader(char **arrayCHR, int N){
+    char charArray[100];
+    for (int i = 0; i<N;i++){
+        printf("Enter Sentence: ");
+        scanf("%s", charArray);
+        array[i] = malloc(strlen(charArray)+1);
+        strcpy(charArray[i], charArray);
+    }
+}
 
+void insertSort(char **arrayCHR, int N){
+    for(int i = 0; i<N; i++){
+        int ithLocal = i;
+        while(ithLocal > 0 && strcmp(arrayCHR[ithLocal-1], arrayCHR[ithLocal]>0)){
+            char *temp = arrayCHR[ithLocal];
+            
+        }
+
+    }
+
+}
 
 
 
@@ -17,8 +38,11 @@ int main(){
     if(ptrToMemory == NULL){
         printf("Memory not allocated");
     }
+    stringreader(ptrToMemory,N);
+    insertSort(ptrToMemory,N);
+    showStr(ptrToMemory,N); 
 
-    
+
     free(ptrToMemory);
 
 }
