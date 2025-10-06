a9b7c80a4f32e8153d29de18e7ecf8840b90b762
Jon <135755963+SoleP12@users.noreply.github.com>
2025-10-06 16:14:15 +0000

    added more stuff

--- HW2CS332/searchHMW2jlplayer.c
+++ HW2CS332/searchHMW2jlplayer.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <dirent.h>
 #include <getopt.h>
+#include <stdlib.h>
 
+
+int listDirectory(){
+    DIR *directory;
+    struct dirent *dirent;
+    directory = opendir("/HMW2CS332");
+
+    if(directory == NULL)
+    {
+        printf("Directory Opening error\n");
+        return 1;
+    }
+    while ((dirent = readdir(directory)) != NULL)
+    {
+        if (dirent -> d_type == DT_REG)
+        {
+            printf("File: %s\n", dirent->d_name);
+        }
+    }
+    
+
+
+
+
+
+
+
+}
 
 
 
@@ -11,13 +40,20 @@ int main (int argc, char *argv[]){
     int opt;
     while((opt = getopt(argc,argv, "Ssf")) != 1){
         switch(opt){
-            case 'S' :break;
+            case 'S':          break;
 
-            case 's' :break;
+            case 's':          break;
 
-            case 'f' :break;
+            case 'f':          break;
+
+            default: printf("This file does not exist"); break;
         }
     }
+    if(optind != argc-1){
+        printf("File does not exist");
+        return 1;
+    }
+    char *fn = argv[optind];
 
      
 }
