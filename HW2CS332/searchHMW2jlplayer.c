df46349c5c3adc3581efddd099ea46951fc9cde0
Jon <135755963+SoleP12@users.noreply.github.com>
2025-10-08 04:47:54 +0000

    added more stuff

--- HW2CS332/searchHMW2jlplayer.c
+++ HW2CS332/searchHMW2jlplayer.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <dirent.h>
 #include <getopt.h>
@@ -7,6 +6,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+
+
+int showFilesOnly = 0;
+long sizeLimit =-1;
 
 int listDirectories(const char *dirname) {
     struct dirent *dirent;
@@ -19,19 +22,101 @@ int listDirectories(const char *dirname) {
 
     int count = 1;
     while ((dirent = readdir(parentDir)) != NULL) {
-        // Skip "." and ".."
         if (dirent->d_name[0] == '.' && 
            (dirent->d_name[1] == '\0' || 
            (dirent->d_name[1] == '.' && dirent->d_name[2] == '\0')))
             continue;
-        printf("[%d] %s\n", count++, dirent->d_name);
+
+        char fullPath[1024];
+        snprintf(fullPath, sizeof(fullPath), "%s/%s", dirname, dirent->d_name);
+
+        struct stat sb;
+        if (stat(fullPath, &sb) == -1) {
+            perror("stat");
+            continue;
+        }
+
+        if (showFilesOnly && S_ISDIR(sb.st_mode))
+            continue;
+        
+        printf("[%d] %s (size: ", count++, dirent->d_name);
+        printSize(fullPath);
+        
+        printf(" ---- Permissions: ");
+        permissions(fullPath);
+        printf(" ---- Last File Access: ");
+        lastAccess(fullPath);
+        printf(")\n");
     }
+
     closedir(parentDir);
     return 0;
 }
 
-int main (int argc, char *argv[]){
 
+int printSize(const char *path){
+    struct stat sb;
+    if (stat(path, &sb) == -1) {
+        perror("stat");
+        return -1;
+    }
+
+    if (S_ISDIR(sb.st_mode)) {
+        printf("0");
+        return 0;
+    } else {
+        printf("%ld", sb.st_size);
+        return sb.st_size;
+    }
+}
+
+int permissions(const char *path){
+     struct stat sb;
+    if (stat(path, &sb) == -1) {
+        perror("stat");
+        return -1;
+    }
+    char perms[11];
+    perms[0] = S_ISDIR(sb.st_mode) ? 'd' : '-';
+    perms[1] = (sb.st_mode & S_IRUSR) ? 'r' : '-';
+    perms[2] = (sb.st_mode & S_IWUSR) ? 'w' : '-';
+    perms[3] = (sb.st_mode & S_IXUSR) ? 'x' : '-';
+    perms[4] = (sb.st_mode & S_IRGRP) ? 'r' : '-';
+    perms[5] = (sb.st_mode & S_IWGRP) ? 'w' : '-';
+    perms[6] = (sb.st_mode & S_IXGRP) ? 'x' : '-';
+    perms[7] = (sb.st_mode & S_IROTH) ? 'r' : '-';
+    perms[8] = (sb.st_mode & S_IWOTH) ? 'w' : '-';
+    perms[9] = (sb.st_mode & S_IXOTH) ? 'x' : '-';
+    perms[10] = '\0';
+
+    printf("%s", perms);
+    return 0;
+
+}
+
+
+int lastAccess(const char *path){
+     struct stat sb;
+    if (stat(path, &sb) == -1) {
+        perror("stat");
+        return -1;
+    }
+
+    char timebuf[100];
+    struct tm *tm_info = localtime(&sb.st_atime);
+    strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M:%S", tm_info);
+    printf("%s", timebuf);
+
+    return 0;
+} 
+
+
+
+
+
+
+
+int main (int argc, char *argv[]){
     int opt;
     while((opt = getopt(argc,argv, "Ssf")) != -1){
         switch(opt){
@@ -40,17 +125,18 @@ int main (int argc, char *argv[]){
                 fprintf(stderr, "Usage: %s -S <directory>\n", argv[0]);
                 return 1;
             }      
-                listDirectories(argv[optind]);   
+                listDirectories(argv[optind]);
                 break;
 
-            case 's':          
+            case 's':  
                 break;
 
-            case 'f':          
+            case 'f':     
+
                 break;
 
             case '?': 
-                fprintf(stderr, "Unknown option: -%c\n", optopt);     
+                fprintf(stderr, "Unknown File Tag: -%c\n", optopt);     
                 break;
 
             default: 
@@ -66,11 +152,8 @@ int main (int argc, char *argv[]){
     printf("(Directory Name): %s\n",fn);
 
 }
-/*/ 
-make run ARGS="-S HMW2CS332" 
 
+/*/ 
 make run ARGS="-S projects"
 
-
 */  
-
