3e7cee262c04f06d2a3ee8dd7ba5471d06edd456
Jon <135755963+SoleP12@users.noreply.github.com>
2025-10-14 20:16:23 +0000

    added more stuff

--- Lab7CS332/jlplayerlab7.c
+++ Lab7CS332/jlplayerlab7.c
@@ -5,33 +5,114 @@
 #include <string.h>
 #include <time.h>
 
-
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    pid_t pid;
+    int status;
 
-    FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL) {
-        perror("fopen");
-        exit(EXIT_FAILURE);
-    }
-
-    FILE *log_fp = fopen("output.log", "w");
-    if (log_fp == NULL) {
-        perror("fopen (log file)");
-        fclose(fp);
-        exit(EXIT_FAILURE);
-    }
-
-    char line[256];
+    FILE *filePtr = fopen(argv[1], "r");
     
-    fclose(fp);
-    fclose(log_fp);
+    if (filePtr == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+    char buffer[256];
+     while (fgets(buffer, sizeof(buffer), filePtr)) {
+        time_t start_time = time(NULL);
+        char *start_str = ctime(&start_time);
 
+        
+        printf("Start Time: %s\n", start_str);
+        printf("Commands: %s", buffer);
+       
+
+        pid_t pid = fork();
+
+        pid = fork();
+        if (pid < 0) {
+        // fork() failed
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+     else if (pid == 0) {
+        // Child process
+        int status = system(buffer);
+        printf("This is the child process [PID: %ld]\n", (long)getpid());
+        exit(WEXITSTATUS(status));
+    }
+    else{
+        int status;    
+        wait(&status);
+        time_t end_time = time(NULL);
+        char *end_str = ctime(&end_time);
+        if (WIFEXITED(status)) {
+            printf("Command exited with status: %d\n", WEXITSTATUS(status));
+        } else {
+            printf("Command did not terminate normally.\n");
+        }
+
+        printf("End Time:   %s\n", end_str);
+        printf("----------------------------------------\n");
+        
+    }
+
+
+
+
+
+
+
+
+    }
+
+    
+   
+    // else {
+    //     // Parent process
+    //     printf("Wait for child process to terminate\n");
+
+    //     wait(&status); // Wait for child to finish
+
+    //     if (WIFEXITED(status)) {
+    //         printf("Child process exited with status = %d\n", WEXITSTATUS(status));
+    //     } else {
+    //         printf("Child process did not terminate normally\n");
+    //     }
+    //     printf("Parent process [PID: %ld]\n", (long)getpid());
+    // }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+    printf("\n");
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    fclose(filePtr);
     return 0;
 }
+
+
+
 
 // gcc -o jlplayerlab7 jlplayerlab7.c
 // ./jlplayerlab7 command.txt
