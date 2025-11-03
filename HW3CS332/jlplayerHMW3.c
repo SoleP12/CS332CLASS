6e79bc6501a360adc438706dfe1202e0695665fc
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-03 00:26:18 +0000

    addedd

--- HW3CS332/jlplayerHMW3.c
+++ HW3CS332/jlplayerHMW3.c
@@ -13,18 +13,24 @@ void getDirectory(const char *directName) {
         perror("opendir");
         return;
     }
-
     while ((dirp = readdir(dp)) != NULL) {
         printf("%s\n", dirp->d_name);
     }
-
     closedir(dp);
 }
 
-
-
-
-
+void listFiles(const char* dirname){
+    DIR* dir = opendir(".");
+    if (dir == NULL){
+        return 1;
+    }
+    struct dirent* entity;
+    entity = readdir(dir);
+    while(entity != NULL){
+        printf("%hhd %s\n", entity->d_type, entity->d_name);
+        entity = readdir(dir);
+    }
+}
 
 
 int main(int argc, char* argv[]){
