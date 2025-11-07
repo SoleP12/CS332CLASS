563216f3b2df46300d2bf4ea9a797be728b3ca80
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-07 15:05:18 +0000

    added

--- Lab10CS332/jlplayerlab10.c
+++ Lab10CS332/jlplayerlab10.c
@@ -1,5 +1,5 @@
 /* Sample solution to Lab-7 assignment.
- * To Compile: gcc -Wall -o lab7 lab7_solution.c
+ * To Compile: gcc -Wall -o jlplayerlab10 jlplayerlab10.c
  * To Run: ./lab7 commands.txt
  */
 #include <stdio.h>
