2f4d4582566b53a4834019aadadf5d7fd6976015
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-12 19:13:43 +0000

    added

--- Lab11CS332/jlplayerlab11.c
+++ Lab11CS332/jlplayerlab11.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int pipe(int pipefd[2]);
 
 
-
-int main(){
-    printf("hello");
-}
-
 int main(int argc, char *argv[]){
-    
+    if (argc != 3) {
+        printf("Usage: %s <command1> <command2>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    scanf("Enter Unix Command: ");
+
 }
