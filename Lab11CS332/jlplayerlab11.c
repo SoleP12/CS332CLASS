3f5fdd2492f0a79d1d003b44a6fe8dd19c78cd9c
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-14 05:40:27 +0000

    added

--- Lab11CS332/jlplayerlab11.c
+++ Lab11CS332/jlplayerlab11.c
@@ -13,28 +13,31 @@ int main() {
     char line[BUFSIZ];
     char command[1024];
 
-    // Initalize Infinite Loop
+    // Initalize Infinite Loop of asking for commands
     while (1) {
         printf("Enter Command (or quit to exit): ");
         scanf(" %[^\n]", command);  // 
 
+        // Stop prompting user if quit entered
         if (strcmp(command, "quit") == 0) {
             printf("Exiting program...bye\n");
             break;
         }
-
+        
+        // Runs the terminal command and opens the pipe
         fp = popen(command, "r");
         if (fp == NULL) {
             perror("popen");
             continue;
         }
 
-        
+        // Gets the output of the command and diplays from the pipe(fp)
+        // prints to terminal
         while (fgets(line, sizeof(line), fp) != NULL) {
             printf("%s", line);
         }
 
-        
+        // Closes the pipe while checking for errors
         if (pclose(fp) == -1) {
             perror("pclose");
         }
