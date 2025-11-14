11b88316edfddcd1fd82de8a487600221db6d0ee
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-14 05:16:13 +0000

    added more

--- Lab11CS332/jlplayerlab11.c
+++ Lab11CS332/jlplayerlab11.c
@@ -2,47 +2,50 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/* Simple program to illustrate how to use popen and pclose system calls.
-*
-* The program reads a command as command-line argument,
-* uses popen to create a pipe in "read" mode,
-* reads the output of the pipe, and writes it to stdout.
-*
+/*
 * To Compile: gcc -Wall jlplayerlab11.c
 * To Run: ./a.out <command>
 */
 
-int main(int argc, char **argv) {
-    FILE *fp1, *fp2;
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *fp;
     char line[BUFSIZ];
-    if (argc != 3) {
-    printf("Usage: %s <command1> <command2>\n", argv[0]);
-    exit(EXIT_FAILURE);
+    char command[1024];
+
+    while (1) {
+        printf("Enter Command (or quit to exit): ");
+        scanf(" %[^\n]", command);  // 
+
+        if (strcmp(command, "quit") == 0) {
+            printf("Exiting...\n");
+            break;
+        }
+
+        // Execute the command
+        fp = popen(command, "r");
+        if (fp == NULL) {
+            perror("popen");
+            continue;
+        }
+
+        // Print command output
+        while (fgets(line, sizeof(line), fp) != NULL) {
+            printf("%s", line);
+        }
+
+        // Close the pipe
+        if (pclose(fp) == -1) {
+            perror("pclose");
+        }
+
+        printf("\n");  // Add spacing between commands
     }
-    /* create a pipe, fork/exec command argv[1], in "read" mode */
-    /* read mode - parent process reads stdout of child process */
-    if ((fp1 = popen(argv[1], "r")) == NULL) {
-    perror("popen");
-    exit(EXIT_FAILURE);
-    }
-    /* create a pipe, fork/exec command argv[2], in "write" mode */
-    /* write mode - parent process writes to stdin of child process */
-    if ((fp2 = popen(argv[2], "w")) == NULL) {
-    perror("popen");
-    exit(EXIT_FAILURE);
-    }
-    /* read stdout from child process 1 and write to stdin of
-    child process 2 */
-    while (fgets(line, BUFSIZ, fp1) != NULL) {
-    if (fputs(line, fp2) == EOF) {
-    printf("Error writing to pipe\n");
-    exit(EXIT_FAILURE);
-    }
-    }
-    /* wait for child process to terminate */
-    if ((pclose(fp1) == -1) || pclose(fp2) == -1) {
-    perror("pclose");
-    exit(EXIT_FAILURE);
-    }
+
     return 0;
 }
