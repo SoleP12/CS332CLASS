448f091f1e019a4f9f4bc9ab25f372bc96ae6c27
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-14 05:30:00 +0000

    added

---
+++ Lab11CS332/README.txt
@@ -0,0 +1,15 @@
+/*
+Name: Jonathan Player
+BlazerId: jlplayer
+Project #: Lab11
+To compile: gcc -Wall jlplayerlab11.c
+To run:  ./a.out
+*/
+
+Description --- This code takes the input of the user in the command line
+                and uses the popen and pclose functions in C. With popen we are able
+                to create a pipe between the calling program and the executed command
+
+Purpose --- The reason behind this lab was to build on the knowledge of pipes that we learned
+            in class and see the usages and power of this technique
+
--- Lab11CS332/jlplayerlab11.c
+++ Lab11CS332/jlplayerlab11.c
@@ -14,37 +14,37 @@
 #include <string.h>
 
 int main() {
+
     FILE *fp;
     char line[BUFSIZ];
     char command[1024];
 
+    // Initalize Infinite Loop
     while (1) {
         printf("Enter Command (or quit to exit): ");
         scanf(" %[^\n]", command);  // 
 
         if (strcmp(command, "quit") == 0) {
-            printf("Exiting...\n");
+            printf("Exiting program...bye\n");
             break;
         }
 
-        // Execute the command
         fp = popen(command, "r");
         if (fp == NULL) {
             perror("popen");
             continue;
         }
 
-        // Print command output
+        
         while (fgets(line, sizeof(line), fp) != NULL) {
             printf("%s", line);
         }
 
-        // Close the pipe
+        
         if (pclose(fp) == -1) {
             perror("pclose");
         }
-
-        printf("\n");  // Add spacing between commands
+        
     }
 
     return 0;
