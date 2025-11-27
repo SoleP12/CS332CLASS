8b2c998c258b33c4b5d783a8b1f70cd1c8bbe8b3
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-27 04:46:06 +0000

    added

--- HW4CS332/jlplayerHMW4CS332.c
+++ HW4CS332/jlplayerHMW4CS332.c
@@ -8,17 +8,21 @@
 
 // gcc -O -Wall jlplayerHMW4CS332.c -lpthread -- To Compile
 //                                            -- To Run
+
+
 int fd[2];
 
 
 #define PARENT_PRODUCER_THREADS 10
-#define MAX_VALUERAND 1000
+#define AMOUNT_OF_RANDINT 500
+#define MAX_VALUE_RAND 1001
 #define CHILD_PRODUCER_THREADS 20
-#define CHILD_NUMBERS_TOR_READ 250
+#define CHILD_NUMBERS_TO_READ 250
 
 
-int generateRadNum(){
-
+int generateRadNum(int **arr, int count){
+    
+    
 }
 
 
@@ -32,6 +36,7 @@ int main(){
     }
 
     pid_t pid = fork();
+    
 
 
 }
