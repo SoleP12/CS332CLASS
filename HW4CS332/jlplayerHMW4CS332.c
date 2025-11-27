345a5b05ce45b19c42dcc9de1fa1c433f2512b87
Jon <135755963+SoleP12@users.noreply.github.com>
2025-11-27 04:05:47 +0000

    more stuff

--- HW4CS332/jlplayerHMW4CS332.c
+++ HW4CS332/jlplayerHMW4CS332.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <time.h>
 
 
+// gcc -O -Wall jlplayerHMW4CS332.c -lpthread -- To Compile
+//                                            -- To Run
+int fd[2];
 
 
+#define PARENT_PRODUCER_THREADS 10
+#define MAX_VALUERAND 1000
+#define CHILD_PRODUCER_THREADS 20
+#define CHILD_NUMBERS_TOR_READ 250
 
+
+int generateRadNum(){
+
+}
 
 
 
 int main(){
+    srand(time(NULL));
+
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+
 
 }
